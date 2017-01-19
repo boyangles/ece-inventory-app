2cf07033a0e78884343d0b9a8b07a002674155c8
Jordan Frazier <jrf30@duke.edu>
2017-01-19 16:43:37 -0500

    Migrate database for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170118051910) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
