ffc2de9faa8eed8e25964f23e8bfef442a43b4cd
Jordan Frazier <jrf30@duke.edu>
2017-01-20 16:06:47 -0500

    Main page goes to welcome index. gonna stop pushing to master now i promise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-
-  root 'static_pages#home'
+  root 'welcome#index'
 end
