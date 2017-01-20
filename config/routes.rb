d6e0635e9e532976fe1a6d67b4f1a5c3ffeb6cbb
Jordan Frazier <jrf30@duke.edu>
2017-01-20 15:22:22 -0500

    Home page is static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'static_pages/home'
+  get 'static_pages/help'
+
   root 'static_pages#home'
 end
