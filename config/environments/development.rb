a3d5bc32395a81bf87beaf1c27a1977d32c1f7f1
Jordan Frazier <jrf30@duke.edu>
2017-01-24 12:05:34 -0500

    hcanged to use env variable for gmail account

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
       :address              => 'smtp.gmail.com',
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'jordan.r.frazier@gmail.com',
-      :password             => 'DogCat!@#',
+      :user_name            => ENV['GMAIL_USERNAME'],
+      :password             => ENV['GMAIL_PASSWORD'],
       :authentication       => 'login',
       :enable_starttls_auto => true
   }
