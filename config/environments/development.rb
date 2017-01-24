249dfd4a3f5da669016db4a6a0b7c543d396b406
Jordan Frazier <jrf30@duke.edu>
2017-01-24 12:01:33 -0500

    got it to send to duke email through local service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,14 +22,23 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
+  # Don't care if the mailer can't send.  -------EMAIL SHIT
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'jordan.r.frazier@gmail.com',
+      :password             => 'DogCat!@#',
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
