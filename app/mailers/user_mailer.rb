fa151e6ddf280f1d7f096956a143d2ae1a8c6b56
Jordan Frazier <jrf30@duke.edu>
2017-01-24 10:18:06 -0500

    adding email shit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ApplicationMailer
+
+  default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,19 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # EMAILS CONFIGURATION
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV['jordan.r.frazier@gmail.com'],
+      :password             => ENV['DogCat!@#'],
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
