8589aa8bebebff6861750b8230bcb5df216719d8
Jordan Frazier <jrf30@duke.edu>
2017-02-02 19:48:07 -0500

    Add a few sign in tests in capybara

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,45 @@
 
 # FactoryGirl.find_definitions
 
-RSpec.describe "home page", :type => :feature do
+RSpec.describe "sign in tests", :type => :feature do
   it "displays the user's username after successful login" do
-    #user = FactoryGirl.create(:user, :username => "jdoe", :password => "secret")
-    user = User.create!(username: "jdoe", email: "jaiefn@duke.edu", password: "password", privilege: "admin",
-                        status: "approved", email_confirmed: "true")
-    visit "/login"
-    fill_in "Username", :with => "jdoe"
-    fill_in "Password", :with => "password"
+    user = FactoryGirl.create(:approved_user)
+    visit login_path
+    fill_in "Username", :with => user.username
+    fill_in "Password", :with => user.password
     click_button "Log in"
+    page.all('a', :text => user.username)
+    expect(page).to have_content user.username
+    expect(page).to have_content user.privilege
+    expect(page).to have_content "Items"
+    expect(page).to have_no_content "Users"
+    expect(page).to have_no_content "Account Requests"
+    expect(page).to have_no_content "Tags"
 
-    page.all('a', :text => 'jdoe')
   end
+
+  it "fails to login with not approved user" do
+    user = FactoryGirl.create(:email_not_confirmed_user)
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    expect(page.current_path).to eq login_path
+    expect(page).to have_content "Please activate your account by following the instructions in the account confirmation email you received to proceed"
+  end
+
+  it "login with admin user" do
+    user = FactoryGirl.create(:admin)
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    expect(page).to have_content user.username
+    expect(page).to have_content user.privilege
+    expect(page).to have_content "Users"
+    expect(page).to have_content "Items"
+    expect(page).to have_content "Account Requests"
+    expect(page).to have_content "Tags"
+  end
+
 end
