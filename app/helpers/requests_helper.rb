0cc100edc1fb3d67e06ed4e175d5963a78c065e4
Austin Liu <austin.by.liu@gmail.com>
2017-02-06 21:08:01 -0500

    fixed invalid view of requests

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,6 @@
 module RequestsHelper
+  def check_requests_corresponds_to_current_user
+    @user = Request.find(params[:id]).user
+    redirect_to(root_url) unless current_user?(@user) || current_user.privilege_admin?
+  end
 end
