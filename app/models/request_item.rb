a34363d9bdbef047841fd76e571e2a18851bd31f
Austin Liu <austin.by.liu@gmail.com>
2017-02-20 23:19:41 -0500

    Fixed foreign key bug for destroy callback for Request

--- app/models/request_item.rb
+++ app/models/request_item.rb
@@ -8,4 +8,6 @@ class RequestItem < ApplicationRecord
   scope :request_id, -> (request_id) { where request_id: request_id }
   scope :item_id, -> (item_id) { where item_id: item_id }
   scope :quantity, -> (quantity) { where quantity: quantity }
+
+  validates :quantity, presence: true, :numericality => {:only_integer => true, :greater_than => 0}
 end
