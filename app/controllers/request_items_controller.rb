2196543da2908cc9ebafde54f2f0f915b95d4ce8
duckgirl07 <bridget.dou@gmail.com>
2017-02-27 17:56:19 -0500

    Can remove items from cart; cannot add duplicate items to cart

--- app/controllers/request_items_controller.rb
+++ app/controllers/request_items_controller.rb
@@ -8,21 +8,36 @@ class RequestItemsController < ApplicationController
       @request_item[:item_id] = params[:item_id]
     end
 
-    # look for cart to link request to item
-    @request = grab_cart(current_user)
-    @request_item[:request_id] = @request.id
+		if !RequestItem.where(item_id: params[:item_id]).where(request_id: grab_cart(current_user).id).take.nil?
+			flash[:success] = "This item has already been added to your cart! Edit cart to increase quantity."
+			redirect_to request_path(grab_cart(current_user).id)
+		else
+    	# look for cart to link request to item
+    	@request = grab_cart(current_user)
+    	@request_item[:request_id] = @request.id
+		end
+
   end
 
   def create
-    @request_item = RequestItem.new(request_item_params) #make private def later
-    if @request_item.save
-			@request = grab_cart(current_user)
-    	redirect_to request_path(@request)
-		else
-      flash.now[:danger] = "You may not add this to the cart!"
-      render 'new'
-    end
+
+   	@request_item = RequestItem.new(request_item_params) #make private def later
+    	if @request_item.save
+				@request = grab_cart(current_user)
+    		redirect_to request_path(@request.id)
+			else
+      	flash.now[:danger] = "You may not add this to the cart!"
+      	render 'new'
+			end
   end
+
+	def destroy
+		reqit = RequestItem.find(params[:id])
+		req = Request.find(reqit.request_id)
+		reqit.destroy!
+		flash[:success] = "Item removed from request!"
+		redirect_to request_path(req)
+	end
 
   private
 
