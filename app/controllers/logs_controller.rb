24b0cbc5caceb667b5ada842ed738937cbd81c09
Jordan Frazier <jrf30@duke.edu>
2017-02-26 23:30:58 -0500

    A fuckin lit as hell IF tree that does searching for logs, probably works but who knows

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -4,25 +4,45 @@ class LogsController < ApplicationController
   before_action :check_logged_in_user
 
   def index
-    if params[:user_search].blank? && params[:item_search].blank?
+
+    if params[:user_search].blank? && params[:item_search].blank? && params[:start_date].blank? && params[:end_date].blank?
       @logs = Log.all.paginate(page: params[:page], per_page: 10)
     else
-      # users = User.filter_by_search(params[:user_search])
-      # items = Item.filter_by_search(params[:item_search])
-      # @logs = Log.joins("JOIN users ON logs.user_id = users.id")
-
       userLogs = Log.where(user_id: User.select(:id).where("username ILIKE ?", "%#{params[:user_search]}%")).paginate(page: params[:page], per_page: 10)
       itemLogs = Log.where(id: ItemLog.select(:log_id).where(item_id: Item.select(:id).where("unique_name ILIKE ?", "%#{params[:item_search]}"))).paginate(page: params[:page], per_page: 10)
+      startLogs = Log.where("created_at >= :date", date: params[:start_date])
+      endLogs = Log.where("created_at <= :date", date: params[:end_date])
+      betweenDatesLogs = Log.where(created_at: params[:start_date]..params[:end_date]).paginate(page: params[:page], per_page: 10)
 
-      if params[:item_search].blank?
-        @logs = userLogs
-        return
-      elsif params[:user_search].blank?
-        @logs = itemLogs
-        return
-      else
-        @logs = Log.where(id: userLogs & itemLogs).paginate(page: params[:page], per_page: 10)
+      if !params[:user_search].blank?
+        firstLayer = userLogs
       end
+
+      if !params[:item_search].blank? && !firstLayer.blank?
+        secondLayer = Log.where(id: firstLayer & itemLogs)
+      elsif !params[:item_search].blank?
+        secondLayer = itemLogs
+      else
+        secondLayer = firstLayer
+      end
+
+      if !params[:start_date].blank? && !secondLayer.blank?
+        thirdLayer = Log.where(id: secondLayer & startLogs)
+      elsif !params[:start_date].blank?
+        thirdLayer = startLogs
+      else
+        thirdLayer = secondLayer
+      end
+
+      if !params[:end_date].blank? && !thirdLayer.blank?
+        fourthLayer = Log.where(id: thirdLayer & endLogs)
+      elsif !params[:end_date].blank?
+        fourthLayer = endLogs
+      else
+        fourthLayer = thirdLayer
+      end
+
+      @logs = Log.where(id: fourthLayer).paginate(page: params[:page], per_page: 10)
 
     end
   end
