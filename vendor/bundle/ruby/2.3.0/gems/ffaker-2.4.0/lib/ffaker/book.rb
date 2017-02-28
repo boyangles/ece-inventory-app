b490cf658d8b7350ca533f31da7c403912fa6b2d
Jordan Frazier <jrf30@duke.edu>
2017-02-28 00:22:22 -0500

    add vendor bundle datepicker shit

---
+++ vendor/bundle/ruby/2.3.0/gems/ffaker-2.4.0/lib/ffaker/book.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+module FFaker
+  module Book
+    extend ModuleUtils
+    extend self
+
+    def title
+      case rand(2)
+      when 0 then simple_title
+      when 1 then title_with_prefix
+      end
+    end
+
+    def genre
+      fetch_sample(GENRES)
+    end
+
+    def author
+      FFaker::Name.name
+    end
+
+    def isbn
+      (rand(24_000_000_000) + 1_000_000_000).to_s
+    end
+
+    def description(sentence_count = 3)
+      FFaker::Lorem.paragraph(sentence_count)
+    end
+
+    def cover(slug = nil, size = '300x300', format = 'png', bgset = nil)
+      FFaker::Avatar.image(slug, size, format, bgset)
+    end
+
+    private
+
+    def simple_title
+      "#{fetch_sample(ADJ_AND_ADV)} #{fetch_sample(NOUNS)}"
+    end
+
+    def title_with_prefix
+      "#{fetch_sample(PREFIXES)} #{simple_title}"
+    end
+  end
+end
