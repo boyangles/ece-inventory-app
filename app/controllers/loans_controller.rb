a1748cafe57cd77775bd072085ee46f9572d3392
duckgirl07 <bridget.dou@gmail.com>
2017-03-23 01:08:58 -0400

    Buttons for loans working

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,14 +1,13 @@
 class LoansController < ApplicationController
   def index		
 
+		requests = Request.where(status: "approved")
+
 		if !is_manager_or_admin? 
-			loansoo = Request.where(user_id: current_user.id)
-		else
-			loansoo = Request.all
+			loansoo = requests.where(user_id: current_user.id)
 		end
 
-		loansaaa = Request.where(status: "approved").where(id: RequestItem.select(:request_id).where("quantity_loan > ?", 0))
-
-		@loans = (loansoo and loansaaa).order(:updated_at).paginate(page: params[:page], per_page: 10)
+		@loans = RequestItem.where("quantity_loan > ?", 0).where(request_id: loansoo.select(:id)).order(:updated_at).paginate(page: params[:page], per_page: 10)
+		
   end
 end
