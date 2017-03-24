6657932c94f11a2711ce46be74656b324a1361e0
duckgirl07 <bridget.dou@gmail.com>
2017-03-24 19:07:18 -0400

    added logs to item view

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,6 @@
 class LogsController < ApplicationController
-
   before_action :check_logged_in_user
-  before_action :check_manager_or_admin, only [:index]
+  before_action :check_manager_or_admin, only: [:index]
 
   def index
 
