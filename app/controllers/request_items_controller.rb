c337c0b74cc577ec0d57b1eb0c1abc5cfd2faf52
Jordan Frazier <jrf30@duke.edu>
2017-03-26 15:39:55 -0400

    Does not allow negative numbers in loan or disburse forms now

--- app/controllers/request_items_controller.rb
+++ app/controllers/request_items_controller.rb
@@ -1,46 +1,51 @@
 class RequestItemsController < ApplicationController
-  before_action :check_logged_in_user
+	before_action :check_logged_in_user
 	before_action :set_new_quantity, only: [:update]
 
-  def new
-    @request_item = RequestItem.new
+	def new
+		@request_item = RequestItem.new
 
-    if !params[:item_id].blank?
-      @request_item[:item_id] = params[:item_id]
-    end
+		if !params[:item_id].blank?
+			@request_item[:item_id] = params[:item_id]
+		end
 
 		if !RequestItem.where(item_id: params[:item_id]).where(request_id: grab_cart(current_user).id).take.nil?
 			flash[:warning] = "This item has already been added to your cart! Edit cart to increase quantity."
 			redirect_to request_path(grab_cart(current_user).id)
 		else
-    	# look for cart to link request to item
-    	@request = grab_cart(current_user)
-    	@request_item[:request_id] = @request.id
+			# look for cart to link request to item
+			@request = grab_cart(current_user)
+			@request_item[:request_id] = @request.id
 		end
 
-  end
+	end
 
-  def create
+	def create
+		@request_item = RequestItem.new(request_item_params)
 
-   	@request_item = RequestItem.new(request_item_params) #make private def later
-    	if @request_item.save!
-				@request = grab_cart(current_user)
-    		redirect_to request_path(@request.id)
-			else
-      	flash.now[:danger] = "You may not add this to the cart!"
-      	render 'new'
-			end
-  end
+		begin
+			@request_item.save!
+			@request = grab_cart(current_user)
+			redirect_to request_path(@request.id)
+		rescue Exception => e
+			flash[:danger] = "You may not add this to the cart! Error: #{e}"
+			redirect_to item_path(Item.find(@request_item.item_id))
+		end
+
+	end
 
 	def update
 		@request_item = RequestItem.find(params[:id])
+
+		failure_alert = ( (request_item_params[:quantity_disburse].to_f < 0 || request_item_params[:quantity_loan].to_f < 0) ? "Quantity cannot be less than 0" : "Failed updating quantity")
 
 		respond_to do |format|
 			if @request_item.update_attributes(request_item_params)
 				format.html { redirect_to @request_item.request, notice: "Item quantity updated successfully."	}
 				format.json { head :no_content }
 			else
-				format.html { redirect_to @request_item.request, alert: "Failed updating quantity" } 
+				flash[:danger] = failure_alert
+				format.html { redirect_to item_path(Item.find(@request_item.item_id)) }
 				format.json { render json: @request_item.errors, status: :unprocessable_entity }
 			end
 		end
@@ -65,29 +70,29 @@ class RequestItemsController < ApplicationController
 		else
 			reqit.return_subrequest(params[:quantity_to_return].to_f)
 			flash[:success] = "Quantity successfully returned!"
-		end		
+		end
 		redirect_to request_path(reqit.request_id)
 	end
 
-	def disburse_loaned 
+	def disburse_loaned
 		reqit = RequestItem.find(params[:id])
 		if (params[:quantity_to_disburse].to_f > reqit.quantity_loan)
 			flash[:danger] = "That's more than are loaned out!"
 		else
 			reqit.disburse_loaned_subrequest(params[:quantity_to_disburse].to_f)
 			flash[:success] = "Quantity successfully disbursed!"
-		end		
+		end
 		redirect_to request_path(reqit.request_id)
 	end
 
 
-  private
+	private
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def request_item_params
-    # Rails 4+ requires you to whitelist attributes in the controller.
-    params.fetch(:request_item, {}).permit(:id, :quantity_loan, :quantity_disburse, :quantity_return, :item_id, :request_id, :quantity_to_return, :quantity_to_disburse)
-  end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def request_item_params
+		# Rails 4+ requires you to whitelist attributes in the controller.
+		params.fetch(:request_item, {}).permit(:id, :quantity_loan, :quantity_disburse, :quantity_return, :item_id, :request_id, :quantity_to_return, :quantity_to_disburse)
+	end
 
 	def set_new_quantity
 		@request_item = RequestItem.find(params[:id])
--- spec/controllers/request_items_controller_spec.rb
+++ spec/controllers/request_items_controller_spec.rb
@@ -34,6 +34,39 @@ RSpec.describe "Item Controller Tests", :type => :feature do
       # needs to be implemented
     end
 
+  end
+
+  feature "Remove Items from cart" do
+
+    before :each do
+      @item = create :item
+      @item2 = create :item
+      @item3 = create :item
+    end
+
+    # It would be better to do exact matches, aka Remove Item has the id: remove_Item_ID on it or something
+    scenario "student deletes item from own cart" do
+      login(:user_student)
+      add_item_to_cart(@item, 10, 10)
+      add_item_to_cart(@item2, 20, 20)
+      add_item_to_cart(@item3, 30, 30)
+      expect(page).to have_content @item.unique_name
+      expect(page).to have_content @item2.unique_name
+      expect(page).to have_content @item3.unique_name
+      click_link('Remove Item', match: :first)
+      expect(page).to have_no_content @item.unique_name
+      expect(page).to have_content @item2.unique_name
+      expect(page).to have_content @item3.unique_name
+      click_link('Remove Item', match: :first)
+      expect(page).to have_no_content @item.unique_name
+      expect(page).to have_no_content @item2.unique_name
+      expect(page).to have_content @item3.unique_name
+      click_link('Remove Item', match: :first)
+      expect(page).to have_no_content @item.unique_name
+      expect(page).to have_no_content @item2.unique_name
+      expect(page).to have_no_content @item3.unique_name
+    end
+
 
 
   end
@@ -48,7 +81,7 @@ RSpec.describe "Item Controller Tests", :type => :feature do
     expect(page).to have_content @item.unique_name
     expect(page).to have_content loan1
     expect(page).to have_content dis1
-    expect(page).to have_selector :button, 'Remove Item'
+    expect(page).to have_selector :link_or_button, 'Remove Item'
 
     visit item_path(@item2)
     loan2 = 1
@@ -57,16 +90,16 @@ RSpec.describe "Item Controller Tests", :type => :feature do
     verify_cart_default_text_fields(@user)
 
     expect(page).to have_content @item.unique_name
-    expect(page).to have_content loan
-    expect(page).to have_content dis
+    expect(page).to have_content loan1
+    expect(page).to have_content dis1
     expect(page).to have_content @item2.unique_name
     expect(page).to have_content loan2
     expect(page).to have_content dis2
-    expect(page).to have_selector :button, 'Remove Item'
+    expect(page).to have_selector :link_or_button, 'Remove Item'
   end
 
   def add_item_to_cart(item, loan_quantity, disburse_quantity)
-    visit item_path(@item)
+    visit item_path(item)
     fill_in("loan_id", with: loan_quantity)
     fill_in("disburse_id", with: disburse_quantity)
     click_button("Add to Cart")
@@ -75,7 +108,7 @@ RSpec.describe "Item Controller Tests", :type => :feature do
 
   def verify_cart_default_text_fields(user)
     expect(page).to have_current_path request_path(Request.find_by(user_id: user.id).id)
-    expect(page).to have_content 'Your Cart' # For some reason, can't find this now, but it's on UI
+    expect(page).to have_content 'Your Cart'
     expect(page).to have_content 'Item Name'
     expect(page).to have_content 'Requested for Loan'
     expect(page).to have_content 'Requested for Disbursement'
