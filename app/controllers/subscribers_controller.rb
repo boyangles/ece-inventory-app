03be34e0ce75814a79e1177b0d072b253083489c
Joshua Xu <joshxu95@gmail.com>
2017-03-27 13:58:02 -0400

    Revert "Ev3 req emails"

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,13 +3,12 @@ class SubscribersController < ApplicationController
   before_action :check_logged_in_user
   before_action :check_manager_or_admin
 
-  before_action :set_subscriber, only: [:destroy]
-
   def index
 
     # not_deactivated_subscribers = Subscriber.user.where.not(status: 'deactivated')
     ## Need to filter out all unsubscribed users. How to do this??? Ask austin
     # Subscriber.filter(:user.status => 'approved')
+
     @subscribers = Subscriber.paginate(page: params[:page], per_page: 10)
   end
 
@@ -24,11 +23,8 @@ class SubscribersController < ApplicationController
   end
 
   def destroy
-    if (@subscriber.destroy)
-      flash[:success] = "Subscriber deleted!"
-    else
-      flash[:danger] = "Unable to destroy subscriber!"
-    end
+    Subscriber.find(sub_params).destroy
+    flash[:success] = "Subscriber deleted!"
     redirect_to subscribers_path
   end
 
@@ -36,7 +32,7 @@ class SubscribersController < ApplicationController
     if Subscriber.where(:user_id => current_user.id).blank?
          # no user record for this id
       @subscriber = Subscriber.new(sub_params)
-      @subscriber.user = current_user
+      @subscriber.user_id = current_user.id
       if @subscriber.save
         flash[:success] = "Subscriber saved"
         redirect_to subscribers_path
@@ -52,9 +48,6 @@ class SubscribersController < ApplicationController
 
 
   private
-  def set_subscriber
-    @subscriber = Subscriber.find(params[:id])
-  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def sub_params
