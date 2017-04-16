2f5004d408b6956252cedf8e9c675efe6c16111f
Austin Liu <austin.by.liu@gmail.com>
2017-04-16 13:47:38 -0400

    modified create_stocks! for Stocks

--- app/models/item.rb
+++ app/models/item.rb
@@ -369,9 +369,8 @@ class Item < ApplicationRecord
     self.save!
   end
 
-  def create_stock(serial_tag)
+  def create_stock!(serial_tag)
     Item.transaction do
-      raise Exception.new('Serial Tag must be exactly 8 characters') if serial_tag.to_s.size != 8
       Stock.create!(serial_tag: serial_tag, item_id: self.id)
       self.update_item_quantity_on_stock_creation
     end
