6cb3f92a30c035fbfe4dca6aa86a176713f505da
duckgirl07 <bridget.dou@gmail.com>
2017-04-16 23:46:43 -0400

    working on asset logging

---
+++ app/models/stock_item_log.rb
@@ -0,0 +1,2 @@
+class StockItemLog < ApplicationRecord
+end
---
+++ db/migrate/20170416220546_create_stock_item_logs.rb
@@ -0,0 +1,10 @@
+class CreateStockItemLogs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stock_item_logs do |t|
+			t.belongs_to :item_log
+			t.belongs_to :stock
+			t.string :old_serial_tag
+			t.string :curr_serial_tag
+    end
+  end
+end
